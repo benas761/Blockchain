1a76fa4f3b0ee20cc63ee65068ab9ea43eb31cde
Benas761 <benasi89@gmail.com>
2020-11-04 10:58:24 +0200

    The blockchain now works!

--- header.h
+++ header.h
@@ -13,4 +13,40 @@
 
 void midHash(std::string& chr32, long long int x[]);
 std::string badHash(std::string in);
+
+class user{
+public:
+    std::string name, publicKey;
+    double balance;
+    user(std::string nm, std::string pk, int b) {
+        name=nm; publicKey=pk; balance=b;
+    }
+    user(){}
+    operator=(user b){
+        name=b.name; publicKey=b.publicKey; balance=b.balance;
+    }
+};
+
+class transaction{
+public:
+    std::string hashID, inKey, outKey;
+    int sum;
+    bool isValid = true;
+    transaction(std::string inPublicKey, std::string outPublicKey, int sum);
+    operator=(transaction b);
+};
+
+class block{
+public:
+    block() {
+        previousHash="";
+        merkelHash=badHash("");
+    }
+    std::string previousHash, merkelHash;
+    int version = 0, difficulty = 3;
+    unsigned long long int nonce=0;
+    std::vector<transaction> transactions;
+    int timestamp = time(0);
+};
+
 #endif // HEADER_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,63 +1,27 @@
 #include "header.h"
 
-const int userNum=10,  transNum=50, blockSize = 25;
+const int maxBalance = 10000, maxTrans = 1000, userNum=10, transNum=1000, blockSize = 25; // blocksize<=transNum
+std::vector<block> blockchain;
 
-class user{
-public:
-    std::string name, publicKey;
-    double balance;
-    user(std::string nm, std::string pk, int b) {
-        name=nm; publicKey=pk; balance=b;
-    }
-    user(){}
-    operator=(user b){
-        name=b.name; publicKey=b.publicKey; balance=b.balance;
-    }
-};
+transaction::transaction(std::string inPublicKey, std::string outPublicKey, int sum){
+    inKey = inPublicKey;
+    outKey = outPublicKey;
+    this->sum = sum;
+    std::string all = inPublicKey + outPublicKey + std::to_string(this->sum);
+    hashID = badHash(all);
+}
 
-class transaction{
-public:
-    std::string hashID, inKey, outKey;
-    int sum;
-
-    bool isValid = true;
-    transaction(std::string inPublicKey, std::string outPublicKey, int sum){
-        inKey = inPublicKey;
-        outKey = outPublicKey;
-        this->sum = sum;
-        std::string all = inPublicKey + outPublicKey + std::to_string(this->sum);
-        hashID = badHash(all); /// bad! you stupid piece of shit, learn what public keys are already...
-    }
-    transaction() {}
-    operator=(transaction b) {
-        inKey = b.inKey;
-        outKey = b.outKey;
-        sum = b.sum;
-        std::string all = b.inKey + b.outKey + std::to_string(b.sum);
-        hashID = badHash(all);
-    }
-};
-
-class block{
-public:
-    block() {
-        previousHash="";
-        merkelHash=""; /// something needs to happen here
-        nonce=0;  /// same
-    }
-    block(std::string previousHash) {
-
-    }
-    std::string previousHash, merkelHash;
-    int version, difficulty = 3;
-    unsigned long long int nonce=0;
-    std::vector<transaction> transactions;
-    int timestamp = time(0);
-};
+transaction::operator=(transaction b) {
+    inKey = b.inKey;
+    outKey = b.outKey;
+    sum = b.sum;
+    std::string all = b.inKey + b.outKey + std::to_string(b.sum);
+    hashID = badHash(all);
+}
 
 void generateUsers(const int n, user users[]) {
     for(int i=0; i<n; i++) {
-        int b = rand()%9900+100;
+        int b = rand()%(maxBalance-100)+100;
         std::string name = "name"+std::to_string(i);
         user t(name, badHash(name), b);
         users[i]=t;
@@ -69,7 +33,7 @@ std::vector<transaction> generateTransactions(const int n, user users[]) {
     for(int i=0; i<n; i++) {
         int a = rand()%userNum, b=rand()%userNum;
         while(b==a && userNum>1) b = rand()%userNum; // to make sure they don't send money between the same user
-        transaction t(users[a].publicKey, users[b].publicKey, rand()%9990+10); // money go from 10 to 1000
+        transaction t(users[a].publicKey, users[b].publicKey, rand()%(transNum-100)+10); // money go from 10 to 1000
         tr.push_back(t);
     }
     return tr;
@@ -94,13 +58,11 @@ std::string merkleRoot(std::vector<transaction> tr) {
 }
 
 block makeBlock(std::vector<transaction> &transactions){
-
     block b;
     for(int i=0; i<blockSize; i++){
         int trLoc = rand()%transNum;
         b.transactions.push_back(transactions[trLoc]);
         transactions.erase(transactions.begin()+trLoc); // these 3 lines took me 2 hours to write...
-
     }
     b.merkelHash = merkleRoot(b.transactions);
     /*block b(previousHash, transactions, merkelHash, version)
@@ -109,14 +71,43 @@ block makeBlock(std::vector<transaction> &transactions){
     return(b);
 }
 
-void mineBlock(std::string prevHash, std::vector<transaction> &tr){
+std::string blockToString(block b){
+    std::string x = std::to_string(b.difficulty)+b.merkelHash+std::to_string(b.nonce);
+    x += b.previousHash+std::to_string(b.timestamp)+std::to_string(b.version);
+    return(x);
+}
+
+void mineBlock(std::vector<transaction> &tr, user users[]){
     block b = makeBlock(tr);
     std::string comp, zeros; zeros.assign(b.difficulty, '0');
     while(true){
-        std::string currHash = badHash(std::to_string(b.difficulty)+b.merkelHash+std::to_string(b.nonce)+b.previousHash+std::to_string(b.timestamp)+std::to_string(b.version));
+        std::string currHash = badHash(blockToString(b));
         comp = currHash;
         comp.resize(b.difficulty);
         if(comp == zeros) {
+            // add the block to the chain
+            b.previousHash = badHash(blockToString(blockchain.back()));
+            blockchain.push_back(b);
+            // execute transactions
+            for(auto it = blockchain.back().transactions.begin(); it!=blockchain.back().transactions.end(); it++){
+                std::string all = (*it).inKey + (*it).outKey + std::to_string((*it).sum);
+                for(int i=0; i<userNum; i++)
+                if(badHash(all) == (*it).hashID){
+                    // find both users
+                    int user1 = -1, user2 = -1;
+                    for(int i = 0; user1 == -1 || user2 == -1 || i<userNum; i++){
+                        if(users[i].publicKey==(*it).inKey) user1 = i;
+                        else if(users[i].publicKey==(*it).outKey) user2 = i;
+                        if(i-1==userNum) std::cout << "problem.\n";
+                    }
+                    if(users[user1].balance>=(*it).sum){
+                        users[user1].balance -= (*it).sum;
+                        users[user2].balance += (*it).sum;
+                    }
+                    else (*it).isValid = false; //tr.push_back(*it); - instead of just putting it back to transaction list, it just invalidates it
+                }
+                else (*it).isValid = false;
+            }
             break;
         }
         else b.nonce++;
@@ -125,10 +116,12 @@ void mineBlock(std::string prevHash, std::vector<transaction> &tr){
 
 int main()
 {
+    srand(time(NULL));
     block genesisBlock;
+    blockchain.push_back(genesisBlock);
     user users[userNum];
-    std::vector<transaction> transactions = generateTransactions(transNum, users);
     generateUsers(userNum, users);
-    mineBlock(badHash("a"), transactions);
+    std::vector<transaction> transactions = generateTransactions(transNum, users);
+    mineBlock(transactions, users);
     return 0;
 }
