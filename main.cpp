59451bdc2869d1dccf751e6c51a45b04a550a4ef
bean <benasi89@gmail.com>
2020-11-26 18:50:16 +0200

    A version that supports Libbitcoin library. Built for Linux.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,163 @@
-#include "header.h"
+//#include "header.h"
+////////////////////////////////////////////////////////
+#include <bitcoin/bitcoin.hpp>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <chrono>
+#include <time.h>
+#include <omp.h>
+#include <limits>
+
+void midHash(std::string& chr32, long long int x[]);
+std::string badHash(std::string in);
+
+class user{
+public:
+    std::string name, publicKey;
+    double balance;
+    user(std::string nm, std::string pk, int b) {
+        name=nm; publicKey=pk; balance=b;
+    }
+    user(){}
+    user operator=(user& b){ // not used in this version
+        name=b.name; publicKey=b.publicKey; balance=b.balance;
+        return *this;
+    }
+};
+
+class transaction{
+public:
+    std::string hashID, inKey, outKey;
+    user *userIn, *userOut;
+    double sum;
+    bool isValid = true;
+    transaction(std::string inPublicKey, std::string outPublicKey, int sum);
+    transaction operator=(transaction b) {
+        inKey = b.inKey;
+        outKey = b.outKey;
+        sum = b.sum;
+        std::string all = b.inKey + b.outKey + std::to_string(b.sum);
+        hashID = badHash(all);
+        return *this;
+    }
+};
+
+class block{
+public:
+    block() {
+        previousHash="";
+        merkelHash=badHash("");
+    }
+    std::string previousHash, merkelHash;
+    int version = 0, difficulty = 3;
+    unsigned long long int nonce=0;
+    std::vector<transaction> transactions;
+    std::vector<std::vector<transaction>::iterator> transPtr;
+    int timestamp = time(0);
+};
+///////////////////////////////////////////////////////////////////
+void midHash(std::string& chr32, long long int x[]){
+    // seperate into 8 ints
+
+    for(int i=0; i<8; i++) {
+        for(int j=i; j<32+i; j+=4)
+            x[i]+=chr32[j%32]+chr32[(j+1)%32]*127+chr32[(j+2)%32]*pow(127, 2)+chr32[(j+3)%32]*pow(127, 3);
+    } // I should probably comment about this...
+
+    // multiply by different primes
+    x[0]*=97; x[1]*=71; x[2]*=67; x[3]*=37;
+    x[4]*=19; x[5]*=31; x[6]*=59; x[7]*=23;
+
+    //square the parts
+    x[0]*=x[0]; x[1]*=x[1]; x[2]*=x[2]; x[3]*=x[3];
+    x[4]*=x[4]; x[5]*=x[5]; x[6]*=x[6]; x[7]*=x[7];
+
+    //get middle 8*1.6 (13) chars
+    std::string sx[8];
+    for(int i=0; i<8; i++) {
+        sx[i]=std::to_string(x[i]); // convert to string
+        if(sx[i].length()<13) sx[i].insert(0, 13-sx[i].length(), '0');// makes sure there IS a 13 character long middle
+        sx[i].erase(0, (sx[i].length()-13)/2); // get the last characters coming after the 13 in the middle
+        sx[i].erase(13);
+        x[i]=stoll(sx[i]);
+    }
+    chr32="";
+}
+
+std::string badHash(std::string in){
+    long long int x[8];
+    for(int i=0; i<8; i++) x[i]=0;
+
+    std::string t = in.substr(0, 32);
+    while(t.length()<32) t+="0";
+    midHash(t, x);
+
+    for(int i=32; i<in.length(); i+=32){
+        t = in.substr(i, 32);
+        while(t.length()<32) t+="0";
+        midHash(t, x);
+    }
+    std::string theHash;
+    std::stringstream ss;
+    ss<<std::setfill('0')<<std::hex;
+    for(int i=0; i<8; i++) {
+        ss<<std::setw(8)<<x[i];
+        std::string t(ss.str());
+        t = t.substr(t.length()-8, 8);
+        theHash+=t;
+        ss.str(std::string());
+    }
+    return theHash;
+}
+//////////////////////////////////////////////////////////////
 
 const int maxBalance = 1000000, maxTrans = 100, userNum=1000, transNum=10000, blockSize = 100; // blocksize<=transNum
 std::vector<block> blockchain;
+
+bc::hash_digest create_merkle(bc::hash_list& merkle) {
+	// Stop if hash list is empty or contains one element
+	if(merkle.empty())
+		return bc::null_hash;
+	else if(merkle.size()==1)
+		return merkle[0];
+	// While there is more than 1 hash in the list, keep looping...
+	while(merkle.size()>1){
+		// If number of hashes is odd, duplicate last hash in the list.
+		if(merkle.size()%2!=0)
+			merkle.push_back(merkle.back());
+		// List size is now even
+		assert(merkle.size()%2==0);
+
+		// New hash list
+		bc::hash_list new_merkle;
+		for(auto it = merkle.begin(); it != merkle.end(); it +=2){
+			// Join both current hashes together (concatenate).
+			bc::data_chunk concat_data(bc::hash_size *2);
+			auto concat = bc::serializer<
+				decltype(concat_data.begin())>(concat_data.begin());
+			concat.write_hash(*it);
+			concat.write_hash(*(it +1));
+			// Hash both of the hashes.
+			bc::hash_digest new_root = bc::bitcoin_hash(concat_data);
+			new_merkle.push_back(new_root);
+		}
+		// This is the new list.
+		merkle = new_merkle;
+		// DEBUG output ------------------------------------- 
+		std::cout <<"Current merkle hash list:"<< std::endl;
+		for(const auto& hash: merkle)
+			std::cout <<"  "<< bc::encode_base16(hash)<< std::endl;
+        std::cout << std::endl;
+        // --------------------------------------------------
+    }
+    // Finally we end up with a single item.
+    return merkle[0];
+}
 
 transaction::transaction(std::string inPublicKey, std::string outPublicKey, int sum){
     inKey = inPublicKey;
@@ -11,20 +167,22 @@ transaction::transaction(std::string inPublicKey, std::string outPublicKey, int 
     hashID = badHash(all);
 }
 
-transaction::operator=(transaction b) {
+/*transaction transaction::operator=(transaction b) {
     inKey = b.inKey;
     outKey = b.outKey;
     sum = b.sum;
     std::string all = b.inKey + b.outKey + std::to_string(b.sum);
     hashID = badHash(all);
-}
+}*/
 
 void generateUsers(const int n, user users[]) {
     for(int i=0; i<n; i++) {
         int b = rand()%(maxBalance-100)+100;
         std::string name = "name"+std::to_string(i);
         user t(name, badHash(name), b);
-        users[i]=t;
+        users[i].name = name;
+        users[i].publicKey = badHash(name);
+        users[i].balance = b;
     }
 }
 
@@ -111,12 +269,13 @@ void addBlock(block b, std::vector<transaction> &transactions, user users[]) {
             else (*it).isValid = false; //tr.push_back(*it); - instead of just putting it back to transaction list, it just invalidates it
         }
         else (*it).isValid = false;
-        std::iter_swap(blockchain.back().transPtr[i], transactions.end()-trDiff-1);
+        std::iter_swap(blockchain.back().transPtr[i], (transactions.end()-trDiff-1));
         trDiff++;
         it++;
     }
     transactions.erase(transactions.end()-trDiff, transactions.end());
 }
+  
 
 int main()
 {
@@ -133,6 +292,7 @@ int main()
         // The block mining competition
         bool blockMined = false;
         const int threadNum = omp_get_max_threads();
+
         block potentialBlocks[threadNum];
         clock_t timestamp[threadNum]; for(int i=0; i<threadNum; i++) timestamp[i]=std::numeric_limits<clock_t>::max();
         #pragma omp parallel
@@ -151,7 +311,7 @@ int main()
 
     std::cout << "Every 100th user's balance, before and after:\n";
     for(int i=0; i<userNum; i+=100) {
-        std::cout << std::setw(6) << sumsBefore[i] << std::setw(6) << users[i].balance << std::endl;
+        std::cout << std::setw(8) << sumsBefore[i] << std::setw(8) << users[i].balance << std::endl;
     }
     return 0;
 }
